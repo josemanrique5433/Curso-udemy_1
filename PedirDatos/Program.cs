4aa79842b2cb0a7f81d69a31300ec3000f7904a6
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-08-16 07:51:28 -0500

    Pedir datos al usuario

---
+++ PedirDatos/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+
+        Console.Write("Digita el primer número: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digita el segundo número: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Su división entera es: ");
+        Console.WriteLine(a / b);
+
+        Console.Write("Su residuo es: ");
+        Console.WriteLine(a % b);
+    }
+}
