ffce58a6eac690a3c8ee2f4766c2867e00d34d2f
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-08-19 11:23:45 -0500

    IF

---
+++ Condicional_If/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+        
+        Console.Write("Digita el primer número: ");
+        a=Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digita el segundo número");
+        b =Convert.ToInt32(Console.ReadLine());
+
+        if (a > b) 
+        {
+            Console.WriteLine("El numero mayor es {0} ", a);
+            
+        }
+        else
+        {
+            Console.WriteLine("el número mayor es {0}", b);
+
+        }
+       
+
+    }
+}
