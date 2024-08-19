5ad9ddf42d72d47d2ec570cf72e05687a1624926
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-08-19 12:11:10 -0500

    ELSE IF

--- Condicional_If/Program.cs
+++ Condicional_If/Program.cs
@@ -3,22 +3,29 @@ class Program
 {
     static void Main()
     {
-        int a, b;
-        
+        int a, b, c;
+
         Console.Write("Digita el primer número: ");
-        a=Convert.ToInt32(Console.ReadLine());
+        a = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Digita el segundo número");
-        b =Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digita el segundo número: ");
+        b = Convert.ToInt32(Console.ReadLine());
 
-        if (a > b) 
+        Console.Write("Digita el tercer número: ");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        if ((a >= b) && (a >= c))
         {
-            Console.WriteLine("El numero mayor es {0} ", a);
-            
+            Console.WriteLine("El numero mayor es:  " + a);
+
         }
+        else if ((b>=a) && (b>=c))
+        {
+            Console.WriteLine("El numero mayor es: "+ b);
+                    }
         else
         {
-            Console.WriteLine("el número mayor es {0}", b);
+            Console.WriteLine("el número mayor es "+ c);
 
         }
        
