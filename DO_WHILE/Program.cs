66476dfbb9c5fdbc3155cc4fa90228e74bcd960a
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-08-22 21:04:48 -0500

    USO DO_WHILE

---
+++ DO_WHILE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int password;
+        
+
+        do
+        {
+            Console.WriteLine("Introduce la contraseña ");
+            password = Convert.ToInt32(Console.ReadLine());
+           
+        }
+
+        while (password != 1234);        {
+            
+            Console.WriteLine("Bienvenido");
+            
+        }
+
+    }
+}
